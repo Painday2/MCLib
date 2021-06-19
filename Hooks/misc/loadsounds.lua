b4b5860727d816b461dcc99db7c5f39c54b2726e
Mako <masterjon902@gmail.com>
2021-06-19 05:44:17 -0500

    sound stuff base, needs stuff to actually be added

---
+++ Hooks/misc/loadsounds.lua
@@ -0,0 +1,4 @@
+log("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHEEEEEEEEEEEEEEEWWWWWWWWWWWWWWWWWWWWWWWWOOOOOOOOOOOOOOOO")
+if Global.game_settings.level_id == "mc_branchbank" then
+    ModCore:new(ModPath .. "sounds.xml", true, true)
+end
